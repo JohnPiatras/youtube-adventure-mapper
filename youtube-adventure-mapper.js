bb3918a1c64700b517141cabd5901681c01af44b
burns <john.piatras@gmail.com>
2018-08-23 14:15:50 +0100

    Rewrite of getTitles() to use chained promises for sequential fetching of youtube video titles. Async retreival results in 403 errors if there are a lot of titles to fetch

--- youtube-adventure-mapper.js
+++ youtube-adventure-mapper.js
@@ -10,6 +10,7 @@ const fetchVideoInfo = require('youtube-info');
 //my modules
 const YTFetchAnnotations = require('./YTFetchAnnotations.js');
 
+//Utility functions
 function inspect(object){
     console.log(util.inspect(object, false, null));
 }
@@ -23,13 +24,6 @@ function getAnnotationVideoIds(annotations){
         });
     return result;
 }
-
-
-//Supply a youtube video url at the command line, if none given use this as the default.
-let video_url = process.argv[2];
-if(!video_url) video_url = 'https://www.youtube.com/watch?v=Jm-Kmw8pKXw&feature=youtu.be';
-
-let video_id = new URL(video_url).searchParams.get('v');
 
 function mapAdventure(url, callback){
     let map = {};
@@ -64,46 +58,36 @@ function mapAdventure(url, callback){
     return map;
 }
 
+
 function getTitles(map){
-    //We've mapped the adventure, now grab the video titles.
-    console.log('Fetching video titles');
-    let async_title_fetch_count = 0;
-
     let video_ids = Object.keys(map);
-    let cur_id = 0;
-    
-    getVideoTitles();
-    
-    function getVideoTitles(){
-        fetchVideoInfo(video_ids[cur_id], (err, info) => {
-            console.log("ID = " + video_ids[cur_id]);
-            if(err){
-                console.log(err);                
-            }else{
-                map[video_ids[cur_id]].name = info.title;
-                console.log("  title = " + info.title);
-            }
-            cur_id++;
-            if(cur_id >= video_ids.length){
-                done();
-            }else{
-                getVideoTitles();
-            }
-        });
-    }
+    let video_title_fetch_chain = Promise.resolve();
 
-
-    function done(){
-        json_map = JSON.stringify(map, undefined, 2);
-        fs.writeFile("./output.json", json_map, function(err) {
-            if(err) {
-                return console.log(err);
-            }
+    console.log("Retreiving titles for " + video_ids.length + " videos...");
+    video_ids.forEach( (id, i) => {
         
-            console.log("Saved results to output.json");
-        }); 
+  
+        video_title_fetch_chain =   video_title_fetch_chain
+                                    .then( () =>    fetchVideoInfo(id)
+                                                    .then( (video_info) => {console.log(i + " : " + id + " : " + video_info.title);map[id].name = video_info.title})
+                                    );
+    });
+
+    video_title_fetch_chain.then( () => {
+        write_json(map);
         write_graph(map);
-    }
+    });
+}
+
+function write_json(map){
+    json_map = JSON.stringify(map, undefined, 2);
+    fs.writeFile("./output.json", json_map, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("Saved results to output.json");
+    }); 
 }
 
 function write_graph(map){
@@ -130,12 +114,12 @@ function write_graph(map){
 }
 
 
+
+//Supply a youtube video url at the command line, if none given use this as the default.
+let video_url = process.argv[2];
+if(!video_url) video_url = 'https://www.youtube.com/watch?v=Jm-Kmw8pKXw&feature=youtu.be';
+
+let video_id = new URL(video_url).searchParams.get('v');
+
 console.log('Calling mapAdventure()');
 let map = mapAdventure(video_url, getTitles);
-
-
-
-//repl.start('> ').context.map = map;
-
-
-
